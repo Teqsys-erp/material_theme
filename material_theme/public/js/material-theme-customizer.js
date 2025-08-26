031a44a49101918eef59c825eeebbe9485a5763e
Teqsys-erp <erp.support@teqsys.in>
2025-08-26 15:21:37 +0530

    Update material-theme-customizer.js default red colour

--- material_theme/public/js/material-theme-customizer.js
+++ material_theme/public/js/material-theme-customizer.js
@@ -1,78 +1,25 @@
 frappe.provide("material.theme");
 
 $(document).on("toolbar_setup", function () {
-	const root = document.documentElement;
-	let theme_mode = root.getAttribute("data-theme-mode");
-	console.log(theme_mode);
-	if (theme_mode !== "material")
-	{
-		return;
-	}
-	var themeColor = localStorage.getItem("ItrostackThemeColor");
-	if(themeColor)
-		applyMaterialTheme(themeColor);
-	render_clear_demo_action();
-
+    const root = document.documentElement;
+    let theme_mode = root.getAttribute("data-theme-mode");
+    if (theme_mode !== "material") {
+        return;
+    }
+    // Set your desired color here
+    applyMaterialTheme("#c00007");
 });
 
-function render_clear_demo_action() {
-	let demo_action = $(
-		`<a class="dropdown-item" onclick="return material.theme.clear_demo()">
-			${__("Change Theme Color")}
-		</a>`
-	);
-
-	demo_action.appendTo($("#toolbar-user"));
-	// initThemeCustomizer();
-}
-
-function applyMaterialTheme(SelectedColor)
-{
-	var r = document.querySelector(':root');
-	const theme = themeFromSourceColor(argbFromHex(SelectedColor), [
-		{
-		  name: "custom-1",
-		  value: argbFromHex(SelectedColor),
-		  blend: true,
-		},
-	  ]);
-	  
-	// Check if the user has dark mode turned on
-	const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-	// Apply the theme to the body by updating custom properties for material tokens
-	applyTheme(theme, {target: document.body});
-	
-	const color = hexFromArgb(theme.schemes.light.primary);
-	localStorage.setItem("ItrostackThemeColor", color);
-	
-	//Setting the primary color for frappe.
-	r.style.setProperty('--primary', color);
-}
-
-material.theme.clear_demo = function () {
-	var themeColor = localStorage.getItem("ItrostackThemeColor");
-	if(!themeColor)
-		themeColor = "#c00007";
-	// new dialog
-	var d = new frappe.ui.Dialog({
-		title: "Select Color",
-		fields: [
-			{
-				label: __("Theme color"),
-				fieldname: "Color",
-				fieldtype: "Color",
-				default: themeColor,
-			},
-		],
-	});
-
-
-
-	d.set_primary_action(__("Set Color"), function () {
-		applyMaterialTheme(d.get_value('Color'));
-		d.hide();
-	});
-
-	d.show();
+function applyMaterialTheme(SelectedColor) {
+    var r = document.querySelector(':root');
+    const theme = themeFromSourceColor(argbFromHex(SelectedColor), [
+        {
+            name: "custom-1",
+            value: argbFromHex(SelectedColor),
+            blend: true,
+        },
+    ]);
+    applyTheme(theme, { target: document.body });
+    const color = hexFromArgb(theme.schemes.light.primary);
+    r.style.setProperty('--primary', color);
 }
